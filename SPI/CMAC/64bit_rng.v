0c149c0d0f757434e51a84c2f0af41163e6fecb2
ElToblerone <christianshroe@gmail.com>
2026-03-03 13:45:59 +0000

    ASG works in testbench

--- SPI/CMAC/64bit_rng.v
+++ SPI/CMAC/64bit_rng.v
@@ -1,8 +1,9 @@
 // INCLUDES
 `include "ASG.v"
+`timescale 1ns/1ps
 
 
-
+// see testbench https://www.edaplayground.com/x/SKWH
 // ASG generates one Bit per clock cycle -> wrapper returns random number after 8*8 clock cycles
 //                                                                             /   \
 //                                                                      Byte_size  Challenge_length
@@ -14,13 +15,15 @@ module asg_64_bit (
                     output logic sampled_done  // high when 64 bits have been sampled
                     );
 
-    // Intialize ASG variables
-    wire [1:0] loadIt = 2'b0;
-    wire       load;
-    wire       enable = 1'b1;
-    wire       newBit;  // ASG output
-    wire       clk;
-    wire       reset;
+    // Intialize ASG variables (careful! signals (hence wires and registers!)
+    logic [1:0] loadIt = 2'b0;
+    logic       load = 1'b0;
+    logic       enable = 1'b1;
+    wire        newBit;  // ASG output
+    logic       clk = 1'b0;
+    logic       reset = 1'b0;
+
+    always #(2) clk = ~clk;
 
     ASG asg1 (
         loadIt,
@@ -32,7 +35,118 @@ module asg_64_bit (
     );
 
     logic [5:0] count; // 6 Bit counter: 0..63 
-    logic sampling;
+    own_challenge <= 6'b0;
+    logic sampling = 1'b0;
+    sampled_done <= 1'b0;
+    enable_sample <= 1'b1;
+
+
+// Initialize ASG (and LSFRs) with non zero seeds
+initial begin
+        // Assert reset
+		reset <= 1'b1;
+        @(posedge clk);
+        
+        // Deassert reset
+        reset <= 1'b0;
+        @(posedge clk);
+
+    	//
+        // fill R1 with zeroes
+    	repeat (31) begin
+            loadIt <= 2'b01;
+    	    enable <= 1'b1;
+    	    load <= 1'b0;
+            @(posedge clk);
+        end
+    	//
+        // seed R1 with b'11101
+    	loadIt <= 2'b01;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+
+    	loadIt <= 2'b01;
+    	enable <= 1'b1;
+    	load <= 1'b0;
+    	@(posedge clk);
+
+    	loadIt <= 2'b01;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+
+        loadIt <= 2'b01;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+
+        loadIt <= 2'b01;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+
+    	// fill R2 with zeroes
+        repeat (127) begin
+        	loadIt <= 2'b10;
+        	enable <= 1'b1;
+    	    load <= 1'b0;
+            @(posedge clk);
+        end
+    
+    	// seed R2 with b'11010
+    	loadIt <= 2'b10;
+    	enable <= 1'b1;
+    	load <= 1'b0;
+        @(posedge clk);
+    	
+    	loadIt <= 2'b10;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+
+    	loadIt <= 2'b10;
+    	enable <= 1'b1;
+    	load <= 1'b0;
+    	@(posedge clk);
+
+        loadIt <= 2'b10;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+
+        loadIt <= 2'b10;
+    	enable <= 1'b1;
+    	load <= 1'b1;
+    	@(posedge clk);
+    	
+    	// fill R3 with zeroes
+        repeat (89) begin
+        	loadIt <= 2'b11;
+        	enable <= 1'b1;
+    	    load <= 1'b1;
+    	    @(posedge clk);
+        end 
+        
+      	// seed R3 with many one's
+    	repeat (60) begin
+    		loadIt <= 2'b11;
+    		enable <= 1'b1;
+    		load <= 1'b1;
+    		#1;
+    	end
+        
+        // Stop loading
+        loadIt <= 2'b00;
+      	load <= 1'b0;
+        #1;
+
+        // Enable the ASG
+        enable <= 1'b1;
+    end
+
+
+
 
     // logic: rng begins once enable_sample gets asserted AND NOT sampling
     always_ff @(posedge clk) 
